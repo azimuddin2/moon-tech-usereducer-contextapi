3eec695f86d6b68f5a9b6d6c81a285527b0a7ed9
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-01-06 00:32:29 +0600

    moon tech use reducer context

---
+++ src/routes/routes.js
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import About from "../pages/About";
+import Cart from "../pages/Cart";
+import Home from "../pages/Home";
+import TopRated from "../pages/TopRated";
+
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: 'about',
+                element: <About></About>
+            },
+            {
+                path: 'top-rated',
+                element: <TopRated></TopRated>
+            },
+            {
+                path: 'cart',
+                element: <Cart></Cart>
+            }
+
+        ],
+    },
+]);
+
+
+export default routes;
