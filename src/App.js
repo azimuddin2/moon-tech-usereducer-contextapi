3f927dfcaaba8ec15ac769c47fb959372bb4c129
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-01-07 16:44:06 +0600

    complete done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import ProductProvider from './context/ProductProvider';
 import routes from './routes/routes';
 
 function App() {
   return (
     <div>
-      <RouterProvider router={routes}></RouterProvider>
+      <ProductProvider>
+        <RouterProvider router={routes}></RouterProvider>
+      </ProductProvider>
     </div>
   );
 }
--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import ProductCard from '../components/ProductCard';
+import { useProducts } from '../context/ProductProvider';
 
 const TopRated = () => {
+    const {
+        state: { products, loading, error }
+    } = useProducts();
+
+    let content;
+
+    if (loading) {
+        content = <button type="button" class="bg-indigo-500 ..." disabled>
+            <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
+            Processing...
+        </button>
+    }
+
+    if (error) {
+        content = <p>Something went wrong!!</p>
+    }
+
+    if (!loading && !error && products.length === 0) {
+        content = <p>Nothing to show product list is empty!</p>
+    }
+
+    if (!loading && !error && products.length) {
+        content = products.filter(product => product.rating >= 4).map(product => <ProductCard
+            key={product._id}
+            product={product}
+        ></ProductCard>)
+    }
+
     return (
-        <div>
-            <h1>This is top rated!!</h1>
+        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-5 max-w-7xl mx-auto my-10'>
+            {
+                content
+            }
         </div>
     );
 };
